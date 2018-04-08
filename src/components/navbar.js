f02d8cf316f9c01cfc00ac7871a039f4a6dfd0fd
Jonathan Tiritilli <31551707+jontiritilli@users.noreply.github.com>
2018-04-08 05:39:22 -0700

    basic navbar completed

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,26 @@
 import React, {Component} from 'react';
+import '../assets/css/index.css';
+import logo from '../assets/images/weather.png'
 
 class Navbar extends Component {
   render(){
     return (
-      <h1>hello world</h1>
+      <nav>
+        <div className="navMain">
+          <img src={logo} alt="Logo" className="logoMain"/>
+          <div className="navTitle"> Reactive Weather </div>
+          <div className="navMenuContainer">
+            <div className="navMenu">
+
+            </div>
+            <div className="burger">
+              <span className='top'></span>
+              <span className='middle'></span>
+              <span className='bottom'></span>
+            </div>
+          </div>
+        </div>
+      </nav>
     )
   }
 }
