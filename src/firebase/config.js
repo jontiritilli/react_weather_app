27f65a4a6eb8a541a0c9bfb96c9b213534c67d44
Jonathan Tiritilli <31551707+jontiritilli@users.noreply.github.com>
2018-04-08 09:06:33 -0700

    added basic firebase config and initialize file. added to gitignore the config keys

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyDlKoCm2lzP3rOkFC1yJaKH_pOlUoTybho",
+  authDomain: "weather-app-445f7.firebaseapp.com",
+  databaseURL: "https://weather-app-445f7.firebaseio.com",
+  projectId: "weather-app-445f7",
+  storageBucket: "",
+  messagingSenderId: "520601957176"
+};
