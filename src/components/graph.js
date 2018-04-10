4107a4b870d3eb342de1fa6e3d4a5342633663e0
Jonathan Tiritilli <31551707+jontiritilli@users.noreply.github.com>
2018-04-10 14:33:11 -0700

    styling updates and fixes

--- src/components/graph.js
+++ src/components/graph.js
@@ -3,23 +3,33 @@ import moment from 'moment';
 import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
 class Graph extends Component {
-  createData(data){
-    return data.map((item, index) => item.main.temp)
+  createData(data, type){
+    if(type === 'temp'){
+      return data.map((item, index) => item.main.temp)
+    }
+    return data.map((item, index) => item.main.humidity)
   }
+  // changeGraph(event){
+  //   this.createData(this.props.data, event.target.className)
+  // }
   dataAvg(data){
     return (data.reduce((s, v) => s+v)/data.length).toFixed(2)
   }
   render(){
-    const data = this.createData(this.props.data);
+    const data = this.createData(this.props.data, null);
     const avg = this.dataAvg(data);
     return (
       <div className={this.props.graph ? 'graph' : 'graphHidden'}>
-        <div>Temperature Trend</div>
+        {/* <div className="graphControls">
+          <button className="temp" onClick={this.changeGraph}>Temp</button>
+          <button className="humidity" onClick={this.changeGraph}>Humidity</button>
+        </div> */}
+        <div className='graphHeader'>Temperature Trend</div>
+        <div className='avgTemp'>Avg. Temp: {avg} °F</div>
         <Sparklines height={120} width={180} data={data}>
             <SparklinesLine color={'red'} />
             <SparklinesReferenceLine type="avg" />
         </Sparklines>
-        <div className='avgTemp'>Average Temperature: {avg} °F</div>
       </div>
     )
   }
